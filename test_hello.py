f8e603ac687930692a601dd974e886f85cff1689
Punit Lad <punitlad@gmail.com>
2020-04-23 15:50:08 -0400

    added dummy test

--- test_hello.py
+++ test_hello.py
@@ -1,10 +1,2 @@
-import pytest
-
-from flaskr import flaskr
-
-@pytest.fixture
-def client():
-    with flaskr.app.test_client() as client:
-        with flaskr.app.app_context():
-            flaskr.init_db()
-        yield client
+def test_function():
+    assert True
